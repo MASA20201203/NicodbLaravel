48aaf29a28f05a39667350fa0d825e3a28784630
masa <koedame032@gmail.com>
2024-01-14 11:33:12 +0900

    fix クリ奨ランキングページにルートを通す、不要なhelloルーティング等は削除

---
+++ NicodbApp/app/Http/Controllers/RankingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RankingController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('ranking');
+    }
+}
--- NicodbApp/routes/web.php
+++ NicodbApp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RankingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return view('hello');
-});
-
-Route::get('/ranking', function () {
-    // return 'Hello World!';
-    return view('ranking');
-});
+Route::get('/', [RankingController::class, 'index']);
